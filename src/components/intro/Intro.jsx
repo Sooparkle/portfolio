bd9a69b0e72adf005e721810468d1e77a867672c
Sooparkle <imsoohanmu@gmail.com>
2025-02-25 21:23:45 +0900

    CSS(intro) : Modified Texst and CSS

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -21,7 +21,7 @@ export const Intro = () => {
         <span className="intro-slogan-text">협업 기반</span> 플레이어<span className="intro-slogan-text-sm">(Player)</span>
       </div>
       <div className="intro-text">
-        매니징을 해보니, 작은 UI/UX의 변화만으로 고객과 사용자가 변화하는 모습을 보고 궁금증 갖고 2023년부터 프론트 개발 공부를 시작했습니다.
+        매니징을 해보니, 작은 UI/UX의 변화만으로 고객과 사용자가 변화하는 모습을 보고 궁금증을 갖고 2023년부터 프론트 개발 공부를 시작했습니다.
       </div>
       {/* <div className="intro-end">Front-End</div> */}
       <div className="intro-allow">
